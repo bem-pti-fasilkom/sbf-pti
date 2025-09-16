4ca940e4e9a268697ef7570b9c8d8b89487009fa
narawaa <nashwa.ghania@ui.ac.id>
2025-09-16 21:59:30 +0700

    docs: readme and naming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://sbf-pti.github.io',
+  url: 'https://bem-pti-fasilkom.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sbf-pti/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
